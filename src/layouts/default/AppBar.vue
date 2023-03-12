652d016674abf76f5e79e01b37d1268124070b65
Hector <52730210+Hector954@users.noreply.github.com>
2023-03-11 21:31:29 -0800

    first commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>My files</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+      <v-main style="height: 500px;">
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
